09e31b60bb06312cfb8c96a520db6dad273f798e
9lokesh <115541760+9lokesh@users.noreply.github.com>
2024-04-30 10:46:56 +0530

    Create tsting.py

---
+++ tsting.py
@@ -0,0 +1,17 @@
+name:python script workflow
+on:
+push:
+branch:
+-main
+jobs:
+run_python_script:
+name:Run hello.py
+runs-on:ubuntu-latest
+steps:
+- name:checkout repository
+uses:action/checkout@y2
+- name set name python@y2
+with:
+python-version:'3.12'#use the python version you need
+- name:run tsting.py
+run:pythontsting.py
